7e614701ec0efb31df2aec316f3c75331138c1fa
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2022-11-07 18:30:02 +0530

    improvement
    Always separate combinational and sequential (ba and nbas) for more efficient synthesis

--- pwm_generator.v
+++ pwm_generator.v
@@ -7,28 +7,32 @@ Output :  PWM output ( pwm )
 
 */
 
-module generate_pwm(input clk, input [0:3]duty, output reg pwm);
+module generate_pwm(input clk, input rst, input [0:3]duty, output reg pwm);
   reg [0:3] count;
   reg [0:3] timep = 4'b0000;
-  always@(posedge clk)
+  reg temp;
+  always@(posedge clk, rst)
   begin
-    if(timep == 4'b1010)
+    timep = timep + 1;
+    if(timep == 4'b1010 | rst == 1'b1)
     begin
-        count <= duty;
+        count = duty;
         timep = 4'b0000;
     end
+    else
+        temp = count;
     if(count > 4'b0000)
     begin
-        pwm <= 1'b1;
+        pwm = 1'b1;
         count = count - 1;
     end
     else 
-        pwm <= 1'b0;
-    timep <= timep + 1;
+        pwm = 1'b0;
   end
+
   always@(duty)
   begin
-    count  <= duty;
+    count  = duty;
     timep = 4'b0000; 
   end
 endmodule

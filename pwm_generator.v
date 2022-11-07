8aeea1c174a215d3b82cf506799d3ae76fe0e26b
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2022-11-07 11:32:32 +0530

    working commit

--- pwm_generator.v
+++ pwm_generator.v
@@ -8,13 +8,27 @@ Output :  PWM output ( pwm )
 */
 
 module generate_pwm(input clk, input [0:3]duty, output reg pwm);
-  reg count = 4'b0000;
+  reg [0:3] count;
+  reg [0:3] timep = 4'b0000;
   always@(posedge clk)
   begin
-    case(duty)
-      4'b0000: if(
+    if(timep == 4'b1010)
+    begin
+        count <= duty;
+        timep = 4'b0000;
+    end
+    if(count > 4'b0000)
+    begin
+        pwm <= 1'b1;
+        count = count - 1;
+    end
+    else 
+        pwm <= 1'b0;
+    timep <= timep + 1;
   end
   always@(duty)
   begin
-    
+    count  <= duty;
+    timep = 4'b0000; 
   end
+endmodule

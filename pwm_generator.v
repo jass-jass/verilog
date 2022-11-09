dc0d83db84d07152f59e309b44811dc1b11a5c70
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2022-11-09 23:11:08 +0530

    Improvement commit

--- pwm_generator.v
+++ pwm_generator.v
@@ -1,38 +1,77 @@
-/*  pwm generator
+/*    Tff 
 
-Inputs :  Clock ( clk ),
-          4 bit Duty ( duty ).
-          
-Output :  PWM output ( pwm )
+Inputs :  clock ( clk ),
+          reset ( rst ),
+          T ( t ).
 
-*/
+Output :  Q ( q )
 
-module generate_pwm(input clk, input rst, input [0:3]duty, output reg pwm);
-  reg [0:3] count;
-  reg [0:3] timep = 4'b0000;
-  reg temp;
-  always@(posedge clk, rst)
+Truth Table:   rst  clk   t      q
+                1    x    x      0
+                0   rise  1      ~q
+                0   rise  0      q
+                0   fall  x      q
+
+*/ 
+module tff(input t, input clk, input rst, output reg q);
+  always@(posedge clk, posedge rst) 
   begin
-    timep = timep + 1;
-    if(timep == 4'b1010 | rst == 1'b1)
+    if(rst)
     begin
-        count = duty;
-        timep = 4'b0000;
-    end
-    else
-        temp = count;
-    if(count > 4'b0000)
-    begin
-        pwm = 1'b1;
-        count = count - 1;
+      q <= 1;
     end
     else 
-        pwm = 1'b0;
+    begin
+      if(t)
+        q <= ~q;
+      else
+        q <= q;
+    end
   end
+endmodule
 
-  always@(duty)
+
+/*    4 bit Asynchronous counter
+
+Inputs :  clock ( clk ),
+          reset ( rst ).
+
+Output :  4 bit count ( count ).
+*/
+module up_counter(input clk, input rst, output [3:0] count);
+  and(eleven, ~count[3], ~count[1], ~count[0]);
+  or(reset, eleven, rst);
+  tff t0(.clk(clk), .t(1'b1), .rst(reset), .q(count[0]));
+  tff t1(.clk(count[0]), .t(1'b1), .rst(reset), .q(count[1]));
+  tff t2(.clk(count[1]), .t(1'b1), .rst(reset), .q(count[2]));
+  tff t3(.clk(count[2]), .t(1'b1), .rst(reset), .q(count[3]));
+endmodule
+
+
+/*    pwm generator
+
+Inputs :  clock ( clk ),
+          reset ( rst ),
+          4 bit duty ( duty ).
+
+Output :  pwm (pwm)
+*/
+module generate_pwm(input clk, input rst, input [3:0] duty, output reg pwm, output reg [3:0] cnt);
+  reg n_off, on;
+  up_counter counter(.clk(clk), .rst(rst), .count({a, b, c, d}));
+  always@(a, b, c, d, duty)
   begin
-    count  = duty;
-    timep = 4'b0000; 
+    cnt = {~a, ~b, ~c, ~d};
+    n_off = (~a^duty[3]) || (~b^duty[2]) || (~c^duty[1]) || (~d^duty[0]); 
+    on = a && b && c && d;
+  end
+  always@(n_off, on)
+  begin
+    if(on)
+    begin
+      pwm <= 1'b1;
+    end
+    else if (~n_off)
+      pwm <= 1'b0;
   end
 endmodule
